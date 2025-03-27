edfaece6ad2b1303fe1516ca1d230ccda3d67bf4
kittyrunrun <pimmada110946@gmail.com>
2025-03-27 11:31:28 +0700

    del wrong code aot in master

--- analyze_aot.py
+++
@@ -1,8 +0,0 @@
-def add_numbers(a, b, c, d, e):
-    result = a + b + c + d + e
-    output_result = f"The sum of {a}, {b}, {c}, {d}, and {e} is {result}"
-    print(output_result)
-    return output_result
-
-add_numbers(1, 2, 3, 4, 5)
-print("Done")
